273c05649b666abae674fbc89fdd2b2aab6835a3
S_Kurahashi <86878523+S-krhs@users.noreply.github.com>
2023-03-20 01:22:53 +0900

    2023/03/20 marquee+package organize

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router} from "react-router-dom"
-import Header from './components/header/Header'
 import Sidebar from './components/sidebar/Sidebar'
 import MainRoutes from './MainRoutes'
+import Marquee from './components/marquee/Marquee'
 
 const App = () => {
   return (
     <>
       <div className="App">
         <Router>
-          <div className='header'><Header /></div>
+          <div className='header'><Marquee /></div>
           <div className='sidebar'><Sidebar /></div>
           <div className='main'><MainRoutes /></div>
         </Router>
---
+++ src/components/marquee/Marquee.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Marquee = () => {
+  return (
+    <>
+      <div className="marquee">
+        <div className="gaming">
+          <h2>ここはくらはしのサイト「かにみそナックルカーブ」です。</h2>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Marquee;
