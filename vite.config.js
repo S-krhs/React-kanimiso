24285539056709a277eee61473ab3650b1fce4c1
S_Kurahashi <86878523+S-krhs@users.noreply.github.com>
2023-03-28 18:01:44 +0900

    2023/03/28 vite build config change

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), splitVendorChunkPlugin()],
   server: {
     port: 5173
   },
+  build: {
+    chunkSizeWarningLimit: 800
+},
 });
